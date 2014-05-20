b85fd5e54900764746bdcc113eab8620c7458741
digigp01 <digigp01@gmail.com>
2014-05-20 14:29:03 +0800

    New Project Upload!

---
+++ SimplePaPa/src/model/MemTypeBean.java
@@ -0,0 +1,39 @@
+package model;
+
+public class MemTypeBean {
+
+	
+	private int mTypeId;
+	private String mType;
+	
+	
+	
+	public int getmTypeId() {
+		return mTypeId;
+	}
+
+
+
+	public void setmTypeId(int mTypeId) {
+		this.mTypeId = mTypeId;
+	}
+
+
+
+	public String getmType() {
+		return mType;
+	}
+
+
+
+	public void setmType(String mType) {
+		this.mType = mType;
+	}
+
+
+
+	public static void main(String[] args) {
+		
+	}
+
+}
