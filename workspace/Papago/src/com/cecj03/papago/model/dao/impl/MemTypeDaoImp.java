6fc4571846192d0ce75e13573f582aacf239cb01
jeff <jeff32191182@gmail.com>
2014-05-30 16:28:22 +0800

    更新店家jquery 店家功能

--- workspace/Papago/src/com/cecj03/papago/model/dao/impl/MemTypeDaoImp.java
+++
@@ -1,8 +0,0 @@
-package com.cecj03.papago.model.dao.impl;
-
-import com.cecj03.papago.model.MemType;
-import com.cecj03.papago.model.dao.MemTypeDao;
-
-public class MemTypeDaoImp extends GenericDaoImpl<MemType> implements MemTypeDao{
-
-}
